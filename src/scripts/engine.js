e4119cfed8142699c7eb9fcbbf13b175699de18a
Matheus <mat.hen.gra@gmail.com>
2023-12-16 21:13:56 -0300

    add flipping functionalities; add victory conditions

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -25,5 +25,30 @@ for (let i = 0; i < emojis.length; i++) {
     let card = document.createElement("div");
     card.classList.add("card");
     card.innerHTML = shuffledEmojis[i];
+    card.onclick = handleClick;
     gameBox.appendChild(card);
+}
+
+function handleClick() {
+    if (openCards.length < 2) {
+        this.classList.add("openCard");
+        openCards.push(this);
+    }
+
+    if (openCards.length === 2) {
+        setTimeout(checkMatch, 1000);
+    }
+}
+
+function checkMatch() {
+    if (openCards[0].innerHTML === openCards[1].innerHTML) {
+        openCards.forEach(card => card.classList.add("matchedCard"));
+    }
+    openCards.forEach(card => {
+        card.classList.remove("openCard");
+    });
+    openCards = [];
+    if (document.querySelectorAll(".matchedCard").length === emojis.length) {
+        alert("Você venceu!");
+    }
 }
